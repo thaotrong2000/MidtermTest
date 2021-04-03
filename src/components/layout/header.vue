9516a2606a9ba06dbfb7f56574a8b1337f399cb3
thaotrong2000 <trongthaouet@gmail.com>
2021-04-04 00:51:49 +0700

    night: 2021-4-4

---
+++ src/components/layout/header.vue
@@ -0,0 +1,11 @@
+<template>
+    <div id="header">
+        header
+    </div>
+</template>
+<script>
+
+</script>
+<style>
+    @import "../../style/layout/css-header.css"; 
+</style>
---
+++ src/components/layout/navbar.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="navbar">
+    <!-- Tao logo -->
+    <div id="navbar-logo">
+      <!-- Tao bieu tuong dieu huong -->
+      <div id="navbar-direction">
+        
+      </div>
+
+      <!-- Tao Logo -->
+      <div id="navbar-logo-misa"></div>
+    </div>
+
+    <!-- Tao thanh Menu -->
+    <div id="navbar-menu">
+        <button class="btn-menu">
+            <div class="btn-menu-icon btn-dashboard"></div>
+            <div class="btn-menu-text ">Tổng quan</div>
+        </button>
+
+        <button class="btn-menu">
+            <div class="btn-menu-icon btn-report"></div>
+            <div class="btn-menu-text ">Báo cáo</div>
+        </button>
+
+        <button class="btn-menu">
+            <div class="btn-menu-icon btn-buy"></div>
+            <div class="btn-menu-text ">Mua hàng</div>
+        </button>
+        <button class="btn-menu">
+            <div class="btn-menu-icon btn-employee"></div>
+            <div class="btn-menu-text ">Danh mục nhân viên</div>
+        </button>
+
+        <button class="btn-menu">
+            <div class="btn-menu-icon btn-employee"></div>
+            <div class="btn-menu-text ">Danh mục khách hàng</div>
+        </button>
+
+        <button class="btn-menu">
+            <div class="btn-menu-icon btn-system"></div>
+            <div class="btn-menu-text ">Thiết lập hệ thống</div>
+        </button>
+    </div>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+
+<style scoped>
+@import "../../style/layout/css-navbar.css";
+</style>
